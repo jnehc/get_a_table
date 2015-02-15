5ef10034528936241d6f80715f6387169f5d619d
jhc <coombsjh@gmail.com>
2015-02-15 02:32:16 -0500

    create user model, controller, and form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215052149) do
+ActiveRecord::Schema.define(version: 20150215070239) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20150215052149) do
     t.string   "picture"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
