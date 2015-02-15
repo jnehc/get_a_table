1979a4b7576b56e66070a6d108ed69fb71cebc74
jhc <coombsjh@gmail.com>
2015-02-15 00:34:19 -0500

    added restaurant model

---
+++ db/migrate/20150215052149_create_restaurants.rb
@@ -0,0 +1,15 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.text :description
+      t.string :address
+      t.string :phone_number
+      t.string :category
+      t.integer :capacity
+      t.string :picture
+
+      t.timestamps null: false
+    end
+  end
+end
