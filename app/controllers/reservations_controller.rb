97baa670be0bae9d312af0a6b79b0b21431e592a
jhc <coombsjh@gmail.com>
2015-02-15 20:27:27 -0500

    add associations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,38 @@
 class ReservationsController < ApplicationController
-  def new
+ def show
+  @reservation = Reservation.find(params[:id])
+end
+
+	def new
+    @reservation = Reservation.new
+  end
+
+  def create
+  	
+  	end
   end
 
   def edit
+    @reservation = Reservation.find(params[:id])
   end
+
+  def update
+    
+    end
+  end
+
+
+	def destroy
+		@reservation = Reservation.find(params[:id])
+		@reservation.destroy
+		redirect_to restaurant_path, notice: "Your reservation was cancelled."
+
+	end
+
+ private
+  def reservation_params
+  	params.require(:reservation).permit(:party_size, :date, :time)
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to products_url, notice: "You are now signed up!"
+      redirect_to restaurants_url, notice: "You are now signed up!"
     else
       render "new"
     end
