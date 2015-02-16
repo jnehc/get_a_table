bc2358a0b7b5b7b10f57bc9d9cb3eaa925d700c8
jhc <coombsjh@gmail.com>
2015-02-15 23:21:36 -0500

    add flash alerts, reorder routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,6 @@ class User < ActiveRecord::Base
 has_many :reservations
  has_secure_password
 
- validates :email, :name, presence: true
- validates :email, confirmation: true
- validates :email_confirmation, presence: true
 end
 
 # This check is performed only if email_confirmation is not nil. 
